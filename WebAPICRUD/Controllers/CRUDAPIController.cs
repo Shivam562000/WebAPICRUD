941cf5b70feb3413633113cfa9f98f5882851478
10709408 <10709408@LTILPUN23801356>
2023-04-26 16:02:22 +0530

    Add project files.

---
+++ WebAPICRUD/Controllers/CRUDAPIController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc.Razor;
+using WebAPICRUD.Models;
+
+namespace WebAPICRUD.Controllers
+{
+    public class CRUDAPIController : ApiController
+    {
+        WebAPIEntities dbObj = new WebAPIEntities();
+
+        [HttpGet]
+        public IHttpActionResult GetEmployee()
+        {
+            List<Employee> empList = dbObj.Employees.ToList();
+            return Ok(empList);
+        }
+
+        [HttpPost]
+        public IHttpActionResult addEmp(Employee e)
+        {
+            //To insert new emp record 
+            dbObj.Employees.Add(e);
+            //To save inserted record
+            dbObj.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ WebAPICRUD/Controllers/CRUDmvcController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebAPICRUD.Models;
+
+namespace WebAPICRUD.Controllers
+{
+    public class CRUDmvcController : Controller
+    {
+        HttpClient client = new HttpClient();
+        // GET: CRUDmvc
+        public ActionResult Index()
+        {
+            List<Employee> empList = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:44306/api/CRUDAPI");
+            var responses = client.GetAsync("CRUDAPI");
+            responses.Wait();
+
+            var responseTest = responses.Result;
+            if (responseTest.IsSuccessStatusCode)
+            {
+                var display = responseTest.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                empList = display.Result;
+            }
+
+            return View(empList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
